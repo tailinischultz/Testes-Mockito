4c113dc6f057f017040c52e159a8f7912ce81277
Lucas Pietro Biasi Rayzer <145388833+LucasRayzer@users.noreply.github.com>
2024-08-29 16:38:42 -0300

    ADD

--- Mockito1/src/test/java/test/TestPedidoService.java
+++ Mockito1/src/test/java/test/TestPedidoService.java
@@ -1,15 +1,18 @@
 package test;
 
+import Mockito_Example.Modelo.Pedido;
 import Mockito_Example.Modelo.Produto;
+import Mockito_Example.Repository.PedidoRepository;
 import Mockito_Example.Repository.ProdutoRepository;
+import Mockito_Example.Service.PedidoService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class TestPedidoService {
-    
+
     ProdutoRepository produtoRepository = Mockito.mock(ProdutoRepository.class);
-    
+    PedidoRepository pedidoRepository = Mockito.mock(PedidoRepository.class);
     @Test
     public void testBuscarProdutoByID(){
         Produto produto = new Produto(0, "Produto 1", 10);
@@ -21,18 +24,31 @@ public class TestPedidoService {
     }
     
     @Test
-    public void testAddProduto(){
-        
-    }
-    
-    @Test
     public void testNovoPedido(){
-        
+
+
     }
     
     @Test
     public void testCalcTotalPedido(){
-        
+        Produto produto1 = new Produto(1, "Produto 1", 10.0);
+        Produto produto2 = new Produto(2, "Produto 2", 20.0);
+
+        //Teste integrando o método de busca pelo ID
+        Mockito.when(produtoRepository.findById(1)).thenReturn(produto1);
+        Mockito.when(produtoRepository.findById(2)).thenReturn(produto2);
+
+        Pedido pedido = new Pedido();
+        pedido.adicionarProduto(produtoRepository.findById(1));
+        pedido.adicionarProduto(produtoRepository.findById(2));
+
+
+        double totalEsperado = 30.0;
+        Assert.assertEquals(totalEsperado, pedido.calcularTotal(), 0);
+
+        //Verifica se os métodos findById foram chamados com os IDs corretos
+        Mockito.verify(produtoRepository, Mockito.times(1)).findById(1);
+        Mockito.verify(produtoRepository, Mockito.times(1)).findById(2);
     }
     
     @Test
